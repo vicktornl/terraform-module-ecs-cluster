834b17eef6522fceee2a07f98dec399f3f78e008
Rob Moorman <rob@moori.nl>
2022-09-12 12:27:12 +0200

    chore: use /aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended/image_id

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "subnets" {
 }
 
 data "aws_ssm_parameter" "ecs_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended/image_id"
 }
 
 locals {

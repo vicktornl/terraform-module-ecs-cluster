9eabd86c839024f3122e1bb92020830b82057d1f
Rob Moorman <rob@moori.nl>
2022-09-12 11:58:40 +0200

    chore: update ssm key

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "subnets" {
 }
 
 data "aws_ssm_parameter" "ecs_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux/recommended/image_id"
 }
 
 locals {

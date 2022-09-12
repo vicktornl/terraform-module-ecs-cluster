b29336af8dddf7d6efff93e2f12cce3063ec0016
Rob Moorman <rob@moori.nl>
2022-09-12 12:29:33 +0200

    fix: instance iam role policy

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "ecs_instance_role" {
+data "aws_iam_policy_document" "ecs_instance_role_assume" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -9,9 +9,26 @@ data "aws_iam_policy_document" "ecs_instance_role" {
   }
 }
 
+data "aws_iam_policy_document" "ecs_instance_role" {
+  statement {
+    actions = [
+      "cloudwatch:PutMetricData",
+      "ec2:DescribeTags",
+    ]
+
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "ecs_instance_role" {
   name               = "${var.name}-ecs-instance-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_instance_role.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_instance_role_assume.json
+}
+
+resource "aws_iam_role_policy" "ecs_instance_role_policy" {
+  name   = "${var.name}-ecs-instance-role"
+  role   = aws_iam_role.ecs_instance_role.id
+  policy = data.aws_iam_policy_document.ecs_instance_role.json
 }
 
 resource "aws_iam_instance_profile" "ecs_instance_role" {

1ef929236515d81ef15e166b291f821cb978d183
Rob Moorman <rob@moori.nl>
2022-09-13 13:32:29 +0200

    feat: optional image_id

--- variables.tf
+++ variables.tf
@@ -22,6 +22,11 @@ variable "subnets" {
   default = []
 }
 
+variable "image_id" {
+  type    = string
+  default = ""
+}
+
 data "aws_ssm_parameter" "ecs_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended/image_id"
 }

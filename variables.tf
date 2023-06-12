7f039a5c8a74151f012d8fb83d9c2747ad2c865a
Rob Moorman <rob@moori.nl>
2023-06-12 14:30:04 +0200

    fix: no arm architecture by default

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "image_id" {
 }
 
 data "aws_ssm_parameter" "ecs_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended/image_id"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
 locals {
